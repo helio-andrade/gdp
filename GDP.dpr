27e3c042324ae6e16f9b1230b30a74ffb5d07fc2
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-12-09 16:57:25 -0300

    Criação

---
+++ GDP.dpr
@@ -0,0 +1,13 @@
+program GDP;
+
+uses
+  Forms,
+  UPrincipal in 'UPrincipal.pas' {formPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TformPrincipal, formPrincipal);
+  Application.Run;
+end.
